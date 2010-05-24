bdcca33bf9bf9a684acfeb8764d2d6db90a90c7d
Miha Rebernik <rasmajx@gmail.com>
2010-05-24 12:55:48 +0200

    Implemented reCAPTCHA for inquiries submitting.

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -2,6 +2,11 @@ class InquiriesController < InheritedResources::Base
   skip_before_filter :authenticate, :only => [:new, :create, :successful]
   
   def create
-    create! { successful_inquiries_path }
+    @inquiry = Inquiry.new params[:inquiry]
+    create! do |success, failure|
+      verify_recaptcha(:model => @inquiry, :message => "Prepisani besedi nista pravilni.")      
+      success.html { successful_inquiries_path }
+      failure.html { render :action => :new }
+    end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,18 @@
-# Be sure to restart your server when you modify this file
+RAILS_GEM_VERSION = '2.3.7' unless defined? RAILS_GEM_VERSION
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'will_paginate'
-  config.gem 'inherited_resources'
+  config.gem 'inherited_resources', :version => "1.0.3"
   config.gem 'paperclip'
   config.gem 'RedCloth'
   config.gem 'haml'
+  config.gem 'recaptcha', :lib => "recaptcha/rails"
   config.time_zone = 'Ljubljana'
 
   config.i18n.default_locale = :sl
 end
+
+ENV['RECAPTCHA_PUBLIC_KEY']  = '6LfzZLoSAAAAADdVC-7yL1OC_Rm_k43Hd8AiWMip'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6LfzZLoSAAAAAN8izDGjZ-6UIpoipk1Y9dUx0tyg'
